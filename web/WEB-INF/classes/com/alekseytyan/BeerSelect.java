a6894f91b058112ae128279e9916f0fe8dbc26d2
AlexeyTyan <alekseytyan45@gmail.com>
2021-03-09 20:41:29 +0300

    Fixing

--- web/WEB-INF/classes/com/alekseytyan/BeerSelect.java
+++ web/WEB-INF/classes/com/alekseytyan/BeerSelect.java
@@ -3,6 +3,7 @@ package com.alekseytyan;
 import javax.servlet.*;
 import javax.servlet.http.*;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 public class BeerSelect extends HttpServlet {
 
@@ -11,8 +12,13 @@ public class BeerSelect extends HttpServlet {
 
     }
 
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+    public void doPost(HttpServletRequest request,
+                       HttpServletResponse response) throws IOException, ServletException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("Loool");
+        out.println("Beer Selection Advice<br>");
+        String c = request.getParameter("color");
+        out.println("<br>Got beer color " + c);
     }
 }
--- web/index.jsp
+++ web/index.jsp
@@ -1,17 +1,15 @@
 <html><body>
-<h1 align=”center”>Beer Selection Page</h1>
-<form method=”POST”
-      action=”SelectBeer.do”>
+<h1>Beer Selection Page</h1>
+<form method="post"
+      action="SelectBeer.do">
   Select beer characteristics<p>
   Color:
-  <select name=”color” size=”1”>
-    <option value=”light”> light </option>
-    <option value=”amber”> amber </option>
-    <option value=”brown”> brown </option>
-    <option value=”dark”> dark </option>
+  <select lang="en-us" name="color" size="1">
+    <option value="light"> light </option>
+    <option value="amber"> amber </option>
+    <option value="brown"> brown </option>
+    <option value="dark"> dark </option>
   </select>
   <br><br>
-  <center>
-    <input type=”SUBMIT”>
-  </center>
+    <input type="submit">
 </form></body></html>
